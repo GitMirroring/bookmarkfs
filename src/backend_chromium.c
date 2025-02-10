GitMirroring/bookmarkfs

commit d1dac54b72164970b98662ffdd36537b97bf0f2b

chore: tidy up includes

src/backend_chromium.c
@@ -37,7 +37,6 @@
 
 #include <fcntl.h>
 #include <iconv.h>
-#include <sys/stat.h>
 #include <unistd.h>
 
 #ifdef BOOKMARKFS_BACKEND_CHROMIUM_WRITE

src/backend_firefox.c
@@ -35,7 +35,6 @@
 #include <string.h>
 
 #include <fcntl.h>
-#include <sys/stat.h>
 #include <unistd.h>
 
 #ifdef BOOKMARKFS_BACKEND_FIREFOX_WRITE

src/bookmarkctl.c
@@ -25,8 +25,6 @@
 #endif
 
 #include <errno.h>
-#include <inttypes.h>
-#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>

src/db.c
@@ -28,7 +28,6 @@
 
 #include <errno.h>
 #include <stdint.h>
-#include <stdio.h>
 #include <string.h>
 
 #include "xstd.h"

src/frontend_util.h
@@ -23,7 +23,6 @@
 #ifndef BOOKMARKFS_FRONTEND_UTIL_H_
 #define BOOKMARKFS_FRONTEND_UTIL_H_
 
-#include <stdint.h>
 #include <stdlib.h>
 
 #include "backend.h"

src/fs_ops.c
@@ -31,6 +31,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <time.h>
 #include <unistd.h>
 

src/fsck.c
@@ -26,7 +26,6 @@
 #  include "config.h"
 #endif
 
-#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>

src/fsck_handler_simple.c
@@ -25,6 +25,7 @@
 #endif
 
 #include <inttypes.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 

commit e199a1203a8813c75431d6006df87a144dd84841

backend: fix includes
Fix a regression in commit d1dac54b72 where sys/stat.h is no longer
included in backend_firefox.c and backend_chromium.c.

It has to be explicitly included for the UTIME_xxx macros.

src/backend_chromium.c
@@ -37,6 +37,7 @@
 
 #include <fcntl.h>
 #include <iconv.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #ifdef BOOKMARKFS_BACKEND_CHROMIUM_WRITE

src/backend_firefox.c
@@ -35,6 +35,7 @@
 #include <string.h>
 
 #include <fcntl.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #ifdef BOOKMARKFS_BACKEND_FIREFOX_WRITE