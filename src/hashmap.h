cadde0b61e1343610b4b028e9b5035b460ce5879
CismonX <admin@cismon.net>
2025-03-06 10:33:15 +0800

    doc: update docs for the utility library
    Move documentation for the hash table from source code comments
    to the user manual.

--- src/hashmap.h
+++ src/hashmap.h
@@ -37,28 +37,15 @@ typedef void (hashmap_walk_func) (
     void *entry
 );
 
-/**
- * Obtain the hashcode for an entry.
- *
- * This function is only called during rehash and entry delete.
- */
 typedef unsigned long (hashmap_hash_func) (
     void const *entry
 );
 
-/**
- * Check if the given key matches an entry in the hashmap.
- *
- * Returns 0 if matches, non-zero if not.
- */
 typedef int (hashmap_comp_func) (
     union hashmap_key  key,
     void const        *entry
 );
 
-/**
- * Creates a new hashmap with given callback functions.
- */
 struct hashmap *
 hashmap_create (
     hashmap_comp_func *entry_comp,
@@ -70,10 +57,6 @@ hashmap_destroy (
     struct hashmap *map
 );
 
-/**
- * Apply the walk_func callback to each entry in hashmap,
- * passing user_data as its second argument.
- */
 void
 hashmap_foreach (
     struct hashmap const *map,
@@ -81,18 +64,6 @@ hashmap_foreach (
     void                 *user_data
 );
 
-/**
- * Search for an entry in the hashmap that matches the given key.
- *
- * If the key matches multiple entries,
- * it is unspecified which one will return.
- *
- * If entry_id_ptr is not NULL, on a successful lookup,
- * it will be set to a value that can be later be passed to
- * hashmap_entry_delete().
- *
- * Returns an entry if found, or NULL if not.
- */
 void *
 hashmap_search (
     struct hashmap const *map,
@@ -101,11 +72,6 @@ hashmap_search (
     unsigned long        *entry_id_ptr
 );
 
-/**
- * Insert an entry into the hashmap.
- *
- * Invalidates all entry IDs given by previous hashmap_search() calls.
- */
 void
 hashmap_insert (
     struct hashmap *map,
@@ -113,14 +79,6 @@ hashmap_insert (
     void           *entry
 );
 
-/**
- * Remove an entry from the hashmap.
- * Undefined behavior if the entry does not exist in hashmap.
- *
- * The entry_id argument should either be the value given by the
- * hashmap_search() or hashmap_insert() function call where the
- * entry is returned from, or -1 (less efficient).
- */
 void
 hashmap_delete (
     struct hashmap *map,
