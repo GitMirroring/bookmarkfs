d810e69334c493d4905cc11273aa4459b05564e0
CismonX <admin@cismon.net>
2026-06-10 08:13:42 +0800

    test: remove hash and prng tests
    These tests does not actually do anything useful, while the
    code we're testing is either copy-pasted or just a library wrapper.
    
    For reliable tests of the said PRNG and hash function, see:
    - https://prng.di.unimi.it/#quality
    - https://github.com/Cyan4973/xxHash/tree/dev/tests

--- tests/check_lib.c
+++ tests/check_lib.c
@@ -22,22 +22,13 @@
 #  include "config.h"
 #endif
 
-#include <inttypes.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include <unistd.h>
-
 #include "check_util.h"
-#include "frontend_util.h"
-#include "hash.h"
-#include "prng.h"
 
 // Forward declaration start
 static int dispatch_subcmds (int, char *[]);
-static int subcmd_hash      (int, char *[]);
-static int subcmd_prng      (int, char *[]);
 // Forward declaration end
 
 static int
@@ -52,11 +43,7 @@ dispatch_subcmds (
     char const *cmd = *(++argv);
 
     int status = -1;
-    if (0 == strcmp("hash", cmd)) {
-        status = subcmd_hash(argc, argv);
-    } else if (0 == strcmp("prng", cmd)) {
-        status = subcmd_prng(argc, argv);
-    } else if (0 == strcmp("watcher", cmd)) {
+    if (0 == strcmp("watcher", cmd)) {
         status = check_watcher(argc, argv);
 #ifdef ENABLE_SANDBOX
     } else if (0 == strcmp("sandbox", cmd)) {
@@ -68,58 +55,6 @@ dispatch_subcmds (
         log_printf("bad subcmd '%s'", cmd);
     }
     return status;
-}
-
-static int
-subcmd_hash (
-    int   argc,
-    char *argv[]
-) {
-    unsigned long long seed = 0;
-
-    OPT_START(argc, argv, "s:")
-    OPT_OPT('s') {
-        seed = strtoull(optarg, NULL, 16);
-        break;
-    }
-    OPT_END
-
-    hash_seed(seed);
-    for (struct hash_ctx *ctx = hash_init(); ; ) {
-        char buf[4096];
-        ssize_t len = xread(STDIN_FILENO, buf, sizeof(buf));
-        if (len < 0) {
-            return -1;
-        }
-        hash_update(ctx, buf, len);
-        if ((size_t)len < sizeof(buf)) {
-            printf("%016" PRIx64 "\n", hash_digest(ctx));
-            return 0;
-        }
-    }
-}
-
-static int
-subcmd_prng (
-    int   argc,
-    char *argv[]
-) {
-    int n = 0;
-
-    OPT_START(argc, argv, "n:")
-    OPT_OPT('n') {
-        n = atoi(optarg);
-        break;
-    }
-    OPT_END
-
-    if (0 != prng_seed_from_env()) {
-        return -1;
-    }
-    for (; n > 0; --n) {
-        printf("%016" PRIx64 "\n", prng_rand());
-    }
-    return 0;
 }
 
 int
